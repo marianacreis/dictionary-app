7b33a601193d936242dd52436313d9c6190c06d0
marianacreis <146851643+marianacreis@users.noreply.github.com>
2024-05-29 11:09:35 +0100

    rendered the word meanings

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,30 +1,37 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Results from "./Results";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
-  function search(event) {
+  function search() {
+    let apiKey = "dd09922bof9dfbd100daf3a044cat44d";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleSubmit(event) {
     event.preventDefault();
-    alert(keyword);
+    search();
   }
 
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
 
-  function handleResponse(response) {}
-
-  let apiKey = "dd09922bof9dfbd100daf3a044cat44d";
-  let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
-  axios.get(apiUrl).then(handleResponse);
+  function handleResponse(response) {
+    setResults(response.data);
+  }
 
   return (
     <div className="Dictionary">
-      <form onSubmit={search}>
+      <form onSubmit={handleSubmit}>
         <input type="search" onChange={handleKeywordChange}></input>
         <input type="submit" value="Search"></input>
       </form>
+      <Results results={results} />
     </div>
   );
 }
---
+++ src/Meaning.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Meaning(props) {
+  return (
+    <div>
+      <h2>{props.meaning.definition}</h2>
+    </div>
+  );
+}
